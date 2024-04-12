66344b6db6beff86e80bd59fcf629ff2e732ad6a
Tomas Pardo Ramos <88736570+tpardo12@users.noreply.github.com>
2024-04-11 21:40:36 -0500

    Update main.py

--- usuarios/main.py
+++ usuarios/main.py
@@ -2,6 +2,7 @@ import random
 import time
 from typing import Union
 
+import uuid
 from faker import Faker
 from fastapi import FastAPI
 from models import usuario
@@ -22,22 +23,26 @@ def read_item(item_id: int, q: Union[str, None] = None):
 @app.post('/saveuser')
 def save_user(post:usuario.User):
     time.sleep(random.uniform(0.5, 2))
-    usuarios.append(post.dict())
+    usuarios.append( usuario.User(id= str(uuid.uuid4()), name= fake.name(), email=fake.email(), phone_number=5448845 ))
     print(usuarios)
     return 'recived'
 
 #update
-@app.put('/usuarios/{usuario_id}')
+@app.put('/users/update/{user_id}')
 def update_user(user_id: str, updateduser: usuario.User):
+  time.sleep(random.uniform(0.5, 2))
+  ''''
     for index, users in enumerate (usuarios):
         if usuarios["id"] == user_id:
             usuarios[index]["name"] =updateduser.name
             usuarios[index]["email"] = updateduser.email
             usuarios[index]["phone_number"] = updateduser.phone_number
             return None
-        
+    '''
+  return usuario.User(id= user_id, name= fake.name(), email=fake.email(), phone_number=5448845 )
+  
 #get
-@app.get('/usuarios/{usuario_id}')
+@app.get('/users/{user_id}')
 def get_user(user_id:str):
-
+    time.sleep(random.uniform(0.5, 2))
     return usuario.User(id= user_id, name= fake.name(), email=fake.email(), phone_number=5448845 )
